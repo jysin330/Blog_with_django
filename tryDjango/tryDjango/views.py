75faabd8e2fe9ea507964d155975c24775097bef
Jyoti <jyotisingh82000@gmail.com>
2023-10-14 14:45:21 +0530

    first migration done

--- tryDjango/tryDjango/views.py
+++ tryDjango/tryDjango/views.py
@@ -1,9 +1,17 @@
 # to render html page
 from django.http import HttpResponse
-HTML_String = ''' <h1>Hello World</h1>'''
-
+from articles.models import Article
+import random
 
 def home(request):
     # taking in a request(Django sends request)
     # return HTML response(We pick to return the response)
-    return HttpResponse(HTML_String)
+    number = random.randint(1,4)
+    # from databases--->
+    article_obj = Article.objects.get(id = number)
+    
+    # django templates--->
+    Article_title = f''' <h1>{article_obj.title} (id : {article_obj.id})</h1>'''
+    Article_content = f''' <h1>{article_obj.content}</h1>'''
+    Html_string= Article_title +  Article_content
+    return HttpResponse(Html_string)
