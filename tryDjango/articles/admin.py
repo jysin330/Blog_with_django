52e3cc616ef61a3c055783cf338796c9c7f1ad5b
Jyoti <jyotisingh82000@gmail.com>
2023-10-15 17:22:56 +0530

    search form and request data

--- tryDjango/articles/admin.py
+++ tryDjango/articles/admin.py
@@ -5,6 +5,6 @@ from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display =['id','title' ,'content']
-    search_fields =['title']
+    search_fields =['title','content']
     
 admin.site.register(Article ,ArticleAdmin)
--- tryDjango/articles/views.py
+++ tryDjango/articles/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Article
-
+def article_search_view(request):
+    query_dict= request.GET #this is dictonary
+    # query = query_dict.get('q') # <input type="text" name="q"/>
+    try:
+        query = int(query_dict.get('q'))
+    except: 
+        query =None
+    article_obj =None
+    if query is not None:
+        article_obj = Article.objects.get(id =query)
+        print(article_obj)
+    else :
+        article_obj ={}
+    context= {
+         'object' : article_obj
+     }
+    return render(request, 'articles/search.html', context = context)
+    
+    
 def article_detail_view(request , id= None):
     
     article_obj =None
