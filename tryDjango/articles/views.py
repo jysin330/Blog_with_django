b7de81152e372c0cabf98500b6df938d41529851
Jyoti <jyotisingh82000@gmail.com>
2023-10-20 21:56:08 +0530

    model forms for article model

--- tryDjango/articles/views.py
+++ tryDjango/articles/views.py
@@ -34,12 +34,14 @@ def article_create_view(request):
         'form' : form
     }
     if form.is_valid():
-            title = form.cleaned_data.get("title")
-            content = form.cleaned_data.get("content")
-            print(title, content)
-            article_object = Article.objects.create(title = title, content= content)
-            context['object'] =article_object
-            context['created'] = True
+            article_object = form.save()
+            context['form'] = ArticleForm()
+            # title = form.cleaned_data.get("title")
+            # content = form.cleaned_data.get("content")
+            # print(title, content)
+            # article_object = Article.objects.create(title = title, content= content)
+            # context['object'] =article_object
+            # context['created'] = True
     return render(request, "articles/create.html", context=context)
 
 
