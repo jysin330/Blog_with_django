f9d3d434aac51c5bd46a53ff66765647401b209f
Jyoti <jyotisingh82000@gmail.com>
2023-10-22 17:05:12 +0530

    login via django authentication form

--- tryDjango/accounts/views.py
+++ tryDjango/accounts/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def login_view (request):
     # if request.user.is_authenticate:
     #     return render(request,'accounts/already-logged-in.html',{})
     if request.method =="POST":
-        username = request.POST.get('username')
-        password =request.POST.get('password')
-        # remove this
-        print(username, password)
+        form = AuthenticationForm(request , data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+            
+        # username = request.POST.get('username')
+        # password =request.POST.get('password')
+        # # remove this
+        # print(username, password)
         
-        user = authenticate(request, username= username, password= password)
+        # user = authenticate(request, username= username, password= password)
         
-        if user is None:
-            context ={"error": "invalid username or password"}
-            return render(request, 'accounts/login.html', context)
+        # if user is None:
+        #     context ={"error": "invalid username or password"}
+        #     return render(request, 'accounts/login.html', context)
         
-        print(user)
-        login(request,user)
-        return redirect('/')
-    return render(request, 'accounts/login.html', {})
+        # print(user)
+           
+    else:
+        form = AuthenticationForm(request )
+            
+    context= {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
 
 
 def logout_view (request):
