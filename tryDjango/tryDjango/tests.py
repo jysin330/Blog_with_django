745d7c3033356bb120c0bc449f8c898b7472dc0e
Jyoti <jyotisingh82000@gmail.com>
2023-12-30 14:01:40 +0530

    automated testing to validate password by djnago built in feature

--- tryDjango/tryDjango/tests.py
+++ tryDjango/tryDjango/tests.py
@@ -1,10 +1,17 @@
 import os
 # from django.conf import settings
 from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
 
 class tryDjangoConfigTest(TestCase):
     def test_secret_key_strength(self):
         # self.assertTrue(1==2)
         # settings.SECRET_KEY
+        # self.assertNotEqual('SECRET_KEY','abc123')
         SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-        self.assertNotEqual('SECRET_KEY','abc123')
+        try:
+            is_strong = validate_password(SECRET_KEY)
+            
+        except Exception as e:
+            msg= f"weak secret key - {e.messages}"
+            self.fail(msg)
