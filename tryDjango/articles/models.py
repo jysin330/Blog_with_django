853cd233947cb96c64f097956cc68a7522d04aa3
Jyoti <jyotisingh82000@gmail.com>
2023-12-30 15:16:51 +0530

    adding timestamp and update field in article model.py

--- tryDjango/articles/models.py
+++ tryDjango/articles/models.py
@@ -4,5 +4,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=80)
     content = models.TextField()
+    timestamp =models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
