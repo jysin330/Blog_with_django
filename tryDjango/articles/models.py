3ff68f636800bb9c434f0cd14ac0a2955d953264
Jyoti <jyotisingh82000@gmail.com>
2024-01-01 21:34:33 +0530

    slug field added

--- tryDjango/articles/models.py
+++ tryDjango/articles/models.py
@@ -5,8 +5,16 @@ from django.utils import timezone
 
 class Article(models.Model):
     title = models.CharField(max_length= 70)
+    slug = models.SlugField(null= True, blank = True)
     content = models.TextField()
     timestamp =models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     # publish =models.DateField(auto_now_add = False, auto_now= False, default = timezone.now)
     publish =models.DateField(auto_now_add = False, auto_now= False, null = True, blank = True)
+
+    def save(self,*args, **kwargs):
+        # obj = Article.objects.get(id=1)
+        # set something
+        super().save(*args, **kwargs)
+        # obj.save()
+        # do another something
