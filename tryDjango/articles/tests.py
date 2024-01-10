64922d660d46c7bd04638a4946081959df289df4
Jyoti <jyotisingh82000@gmail.com>
2024-01-10 20:24:06 +0530

    checking test for queryset exists and queryset count

--- tryDjango/articles/tests.py
+++ tryDjango/articles/tests.py
@@ -6,7 +6,12 @@ from .models import Article
 class ArticleTestCase(TestCase):
     def setUp(self):
         Article.objects.create(title ="hello world", content="another hello world content")
-        
+
     def test_querySet_exists(self):
         qs = Article.objects.all()
         self.assertTrue(qs.exists())
+
+
+    def test_querySet_count(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(),1)
