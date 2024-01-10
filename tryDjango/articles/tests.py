bcf01457e9c8f1d486488c8c281d0a6bd6448932
Jyoti <jyotisingh82000@gmail.com>
2024-01-10 17:53:51 +0530

    setUp for entering into database for testing the Article Model

--- tryDjango/articles/tests.py
+++ tryDjango/articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Article
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        Article.objects.create(title ="hello world", content="another hello world content")
+        
+    def test_querySet_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
