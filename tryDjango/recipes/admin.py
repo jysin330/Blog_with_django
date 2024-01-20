10fafc633081113460aaf66e6a328f9f52131625
Jyoti <jyotisingh82000@gmail.com>
2024-01-20 16:02:08 +0530

    moving Recipe And RecipeIngredient Models into Admin

--- tryDjango/recipes/admin.py
+++ tryDjango/recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Recipe, RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "user"]
+    readonly_fields = ["timestamp", "updated"]
+    raw_id_fields =["user"]
+admin.site.register(Recipe, RecipeAdmin)
+
+
+admin.site.register(RecipeIngredient)
