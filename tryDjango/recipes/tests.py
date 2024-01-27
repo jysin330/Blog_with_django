94f68c3ff06367710706df54b681a9fc8c646116
Jyoti <jyotisingh82000@gmail.com>
2024-01-27 15:05:21 +0530

    Running UserTestCase

--- tryDjango/recipes/tests.py
+++ tryDjango/recipes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        # create_user() is a custom model manager Method That only the user class has
+        self.user_a = User.objects.create_user( "jyoti", password = '1234') 
+
+    def test_user_pw (self):
+        checked = self.user_a.check_password('1234')
+        self.assertTrue(checked)
