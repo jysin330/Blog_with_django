efebf02639fa18270cbc20562f501cb7cb2a4985
Jyoti <jyotisingh82000@gmail.com>
2024-01-27 15:12:30 +0530

    running RecipeTestCase

--- tryDjango/recipes/tests.py
+++ tryDjango/recipes/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 # Create your tests here.
+from .models import Recipe, RecipeIngredient
 User = get_user_model()
 
 class UserTestCase(TestCase):
@@ -11,3 +12,16 @@ class UserTestCase(TestCase):
     def test_user_pw (self):
         checked = self.user_a.check_password('1234')
         self.assertTrue(checked)
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user( "jyoti", password = '1234') 
+
+    def test_user_count(self):
+        qs = User.objects.all()
+        self.assertEqual(qs.count(), 1)
+    
+    def test_user_recipe_reverse_count(self):
+        user = self.user_a
+        qs = user.recipe_get.all()
+        self.assertEqual(qs.count(), 0)
