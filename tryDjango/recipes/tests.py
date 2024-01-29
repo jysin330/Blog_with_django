3c81c5623fd47f6414224053e8739c0803ca2e42
Jyoti <jyotisingh82000@gmail.com>
2024-01-29 17:05:37 +0530

    Test Custom Model Validation Exception

--- tryDjango/recipes/tests.py
+++ tryDjango/recipes/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 # Create your tests here.
 from .models import Recipe, RecipeIngredient
 User = get_user_model()
-
+from django.core.exceptions import ValidationError
 class UserTestCase(TestCase):
     def setUp(self):
         # create_user() is a custom model manager Method That only the user class has
@@ -75,8 +75,31 @@ class RecipeTestCase(TestCase):
         qs = RecipeIngredient.objects.filter(id__in = recipeIngredient_ids)
         self.assertEqual(qs.count(), 1)
 
-    def test_user_two_level_relation_viq_recipes(self):
+    def test_user_two_level_relation_via_recipes(self):
         user =self.user_a
         ids = user.recipe_set.all().values_list('id', flat = True)
         qs = RecipeIngredient.objects.filter(recipe__id__in = ids)
         self.assertEqual(qs.count(), 1)
+
+    def test_unit_measure_validation(self):
+        invalid_unit ='ounce'
+        
+        ingredient = RecipeIngredient(
+                name = 'New',
+                quantity = 10,
+                recipe = self.recipe_a,
+                unit = invalid_unit,
+            )
+        ingredient.full_clean()
+
+    def test_unit_measure_validation_error(self):
+        invalid_unit =['nada', 'havad']
+        with self.assertRaises(ValidationError):
+            for unit in invalid_unit:
+                ingredient = RecipeIngredient(
+                    name = 'New',
+                    quantity = 10,
+                    recipe = self.recipe_a,
+                    unit = unit,
+                )
+                ingredient.full_clean()
